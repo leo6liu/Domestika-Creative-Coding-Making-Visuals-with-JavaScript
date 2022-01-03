0a05a83d286871b687e38534111f9897f99e697d
Leo <leo.battalora@gmail.com>
2022-01-03 15:25:13 -0500

    animate arcs

--- U3-Sketch-Transform/sketch-02.js
+++ U3-Sketch-Transform/sketch-02.js
@@ -3,26 +3,35 @@ const math = require('canvas-sketch-util/math');
 const random = require('canvas-sketch-util/random');
 
 const settings = {
-  dimensions: [1080, 1080]
+  dimensions: [1080, 1080],
+  animate: true,
 };
 
-/*
-const degToRad = (degrees) => {
-  return degrees / 180 * Math.PI;
-}
+const sketch = ({ context, width, height }) => {
+  const arcAgents = [];
+  const numArcAgents = 32;
 
-const randomRange = (min, max) => {
-  return Math.random() * (max - min) + min;
-}
-*/
+  for (let i = 0; i < numArcAgents; i++) {
+    const r = width * 0.3 * random.range(0.5, 1.3);
+    const a = random.range(0, 2 * Math.PI);
+    const arcLength = math.degToRad(360 / numArcAgents) * random.range(0.5, 1.5);
+    const lineWidth = random.range(5, 20);
+    const velocity = random.range(-0.03, 0.03);
 
-const sketch = () => {
+    arcAgents.push(new ArcAgent(r, a, arcLength, lineWidth, velocity));
+  }
+
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
     context.fillStyle = 'black';
     context.strokeStyle = 'black';
+
+    arcAgents.forEach(arcAgent => {
+      arcAgent.update();
+      arcAgent.draw(context, width, height);
+    });
 
     const cx = width * 0.5;
     const cy = height * 0.5;
@@ -34,6 +43,7 @@ const sketch = () => {
     let num = 32;
     let radius = width * 0.3;
 
+    
     for (let i = 0; i < num; i++) {
       const slice = math.degToRad(360 / num);
       const angle = slice * i;
@@ -51,19 +61,10 @@ const sketch = () => {
       context.rect(random.range(0, -w * 0.5), -h * 0.5, w, h);
       context.fill();
       context.restore();
-
-      context.save();
-      context.translate(cx, cy);
-      context.rotate(angle);
-
-      context.lineWidth = random.range(5, 20);
-
-      context.beginPath();
-      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -4), slice * random.range(1, 3));
-      context.stroke();
-      context.restore();
     }
+    
 
+    /*
     num = 4.2;
     radius = width * 0.4;
 
@@ -85,7 +86,51 @@ const sketch = () => {
       context.fill();
       context.restore();
     }
+    */
   };
 };
 
 canvasSketch(sketch, settings);
+
+class PolarCoordinate {
+  constructor(r, a) {
+    this.r = r; // radial coordinate (pixels)
+    this.a = a; // angular coordinate (radians)
+  }
+}
+
+class ArcAgent {
+  constructor(r, a, arcLength, lineWidth, velocity) {
+    this.pos = new PolarCoordinate(r, a);
+    this.arcLength = arcLength;
+    this.lineWidth = lineWidth;
+    this.vel = velocity;
+  }
+
+  update() {
+    this.pos.a += this.vel;
+  }
+
+  draw(context, width, height) {
+    const cx = width * 0.5;
+    const cy = height * 0.5;
+
+    context.save();
+    context.translate(cx, cy);
+    context.lineWidth = this.lineWidth;
+    context.beginPath();
+    context.arc(0, 0, this.pos.r, this.pos.a, this.pos.a + this.arcLength);
+    context.stroke();
+    context.restore();
+  }
+}
+
+/*
+const degToRad = (degrees) => {
+  return degrees / 180 * Math.PI;
+}
+
+const randomRange = (min, max) => {
+  return Math.random() * (max - min) + min;
+}
+*/
